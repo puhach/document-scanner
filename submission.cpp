01411bc05686bc0760c45607710f5f033a25c47d
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-12-21 21:34:44 +0200

    Create a DocumentScanner class

--- submission.cpp
+++ submission.cpp
@@ -9,15 +9,34 @@
 #include <opencv2/highgui.hpp>
 
 
+class DocumentScanner
+{
+public:
+	DocumentScanner() = default;
+
+	// TODO: implement copy/move semantics
+
+	cv::Mat rectify(const cv::Mat& src);	// TODO: consider making it const
+};	// DocumentScanner
+
+cv::Mat DocumentScanner::rectify(const cv::Mat& src)
+{
+	
+}
+
 int main(int argc, char* argv[])
 {
 	try
 	{
 		cv::Mat imSrc = cv::imread("./images/scanned-form.jpg", cv::IMREAD_UNCHANGED);	// TODO: not sure what reading mode should be used
-		cv::imshow("Input", imSrc);
+		/*cv::imshow("Input", imSrc);
 		cv::waitKey();
 
-		cv::destroyAllWindows();
+		cv::destroyAllWindows();*/
+
+		DocumentScanner scanner;
+		scanner.rectify(imSrc);		// TODO: perhaps, pass a file name as a window title
+
 		return 0;
 	}
 	catch (const std::exception& e)
